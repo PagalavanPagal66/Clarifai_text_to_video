7e2f02bfcf78b0cb82614f8d747510d60a62f421
Pagalavan <125946240+PagalavanPagal66@users.noreply.github.com>
2023-08-27 22:56:01 +0530

    Update Clarifai_test.py

--- Clarifai_test.py
+++ Clarifai_test.py
@@ -66,7 +66,7 @@ if(st.button("SUBMIT")):
     for iter in image_prompt:
         st.write(iter)
     st.title("NARRATOR")
-    for iter in narrator_promt:
+    for iter in narrator_prompt:
         st.write(iter)
 
 
