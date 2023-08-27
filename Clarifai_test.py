f56ac415e81ac9d296a6489d9ccfb66c8c44d47a
Pagalavan <125946240+PagalavanPagal66@users.noreply.github.com>
2023-08-27 23:05:13 +0530

    Update Clarifai_test.py

--- Clarifai_test.py
+++ Clarifai_test.py
@@ -68,5 +68,45 @@ if(st.button("SUBMIT")):
     st.title("NARRATOR")
     for iter in narrator_prompt:
         st.write(iter)
-
-
+    PAT = '29d9803eb6f34718ba01d597a4a4e3b5'
+    USER_ID = 'stability-ai'
+    APP_ID = 'stable-diffusion-2'
+    MODEL_ID = 'stable-diffusion-xl'
+    MODEL_VERSION_ID = '0c919cc1edfc455dbc96207753f178d7'
+    TEXT_FILE_URL = 'https://samples.clarifai.com/negative_sentence_12.txt'
+    from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
+    from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
+    from clarifai_grpc.grpc.api.status import status_code_pb2
+    channel = ClarifaiChannel.get_grpc_channel()
+    stub = service_pb2_grpc.V2Stub(channel)
+    metadata = (('authorization', 'Key ' + PAT),)
+    userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID)
+    post_model_outputs_response = stub.PostModelOutputs(
+        service_pb2.PostModelOutputsRequest(
+            user_app_id=userDataObject,
+            model_id=MODEL_ID,
+            version_id=MODEL_VERSION_ID,
+            inputs=[
+                resources_pb2.Input(
+                    data=resources_pb2.Data(
+                        text=resources_pb2.Text(
+                            raw = "A person doing yoga poses on a serene beach at sunrise."
+                        )
+                    )
+                )
+            ]
+        ),
+        metadata=metadata
+    )
+    if post_model_outputs_response.status.code != status_code_pb2.SUCCESS:
+        print(post_model_outputs_response.status)
+        raise Exception("Post model outputs failed, status: " + post_model_outputs_response.status.description)
+    output = post_model_outputs_response.outputs[0]
+    print("Predicted concepts:")
+    for concept in output.data.concepts:
+        print("%s %.2f" % (concept.name, concept.value))
+    base64_image = output.data.image.base64
+    with open("Clarifai_image.jpg",'wb') as f:
+        f.write(base64_image)
+    
+    
